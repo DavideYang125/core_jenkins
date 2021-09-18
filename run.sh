47e8c3389b5b54d1624ae5b6dbb229d62f420a36
yangww <dayang_techmail@163.com>
2021-09-18 18:07:02 +0800

    config

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-mkdir /var/jenkins_home
+if [ ! -d /var/jenkins_home  ];then
+  mkdir /var/jenkins_home
+else
+  echo dir exist
+fi
 
 chown -R 1000 /var/jenkins_home
 
-
-docker-compose -f docker-compose.yml up --build -d --force-recreate
+docker-compose -f docker-compose.yaml up --build -d --force-recreate
 docker system prune -f
